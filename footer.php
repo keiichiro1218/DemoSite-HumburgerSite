14588a7d2eb2d0e246c37606aedda7a6fc48228b
千葉桂一郎 <chibakeiichirou@chibakeiichirounoMacBook-Air.local>
2024-06-18 12:12:26 +0900

    不要なコードの整理

--- footer.php
+++ footer.php
@@ -1,17 +1,14 @@
     <footer class="l-footer">
         <div class="p-footer">
             <div class="p-footer__nav">
-                <!-- <a href="" class="p-footer__link">ショップ情報</a>
-                <a href="" class="p-footer__link">ヒストリー</a> -->
                 <?php wp_nav_menu ( array(
                     'theme_location' => 'footer_menu',
                     'container' => false,
                     'menu_class' => 'p-footer__menu'
                 )) ?>
-
             </div>
             <!-- .p-footer__menu -->
-            <div class="p-footer__Copyright u-fs_sentence">Copyright: RaiseTech</div>
+            <di class="p-footer__Copyright u-fs_sentence">Copyright: RaiseTech</di>
         </div>
         <!-- /.p-footer -->
     </footer>
